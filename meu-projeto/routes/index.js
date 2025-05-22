70844b28e9b98cbb70f8c83ada2dc0f24c65ea5d
pvitoralmeida <paulo.almeida@sou.inteli.edu.br>
2025-05-22 10:45:07 -0300

    commit

--- meu-projeto/routes/index.js
+++ meu-projeto/routes/index.js
@@ -1,9 +1,12 @@
-const express = require("express");
+// routes/index.js
+const express = require('express');
 const router = express.Router();
+const TarefaController = require('../controllers/tarefaController');
 
-// Rota principal
-router.get("/", (req, res) => {
-  res.send("Rota principal funcionando!");
-});
+// Rotas para o CRUD de tarefas
+router.post('/tarefas', TarefaController.criarTarefa);
+router.get('/tarefas', TarefaController.listarTarefas);
+router.put('/tarefas/:id', TarefaController.editarTarefa);
+router.delete('/tarefas/:id', TarefaController.excluirTarefa);
 
 module.exports = router;
